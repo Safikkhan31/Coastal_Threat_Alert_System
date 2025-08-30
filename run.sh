a4a911cfec37219f32ec046cf41c8796a078c57f
Safikkhan31 <safikbihari2@gmail.com>
2025-08-31 00:58:17 +0530

    updated docker file

--- run.sh
+++ run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-while true; do
-    # Start sensor generator in background
-    python3 sensor_data_generator/sensor.py
+# Start sensor generator in background
+python3 sensor_data_generator/sensor.py
 
-    # Start AI models in background
-    python3 AI_models/DatabaseManagment.py
+# Start AI models in background
+python3 AI_models/DatabaseManagment.py
 
-    # Start Java alert service in foreground
-    java -jar app.jar
-    sleep 86400   # wait 24 hours
-done
+# Start Java alert service in foreground
+java -jar app.jar
+
 
