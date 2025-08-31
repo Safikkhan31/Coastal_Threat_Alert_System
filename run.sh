8a2abf8b320a8174d2b2275e847e39bf65ac2fd3
Safikkhan31 <safikbihari2@gmail.com>
2025-08-31 10:41:55 +0530

    a

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 while true; do
     # Start sensor generator in background
-    python3 sensor_data_generator/sensor.py
+    python sensor_data_generator/sensor.py
 
     # Start AI models in background
-    python3 AI_models/DatabaseManagment.py
+    python AI_models/DatabaseManagment.py
 
     # Start Java alert service in foreground
-    java -jar app.jar
+    java -jar ./messege/target/messege-1.0-SNAPSHOT.jar
     sleep 86400   # wait 24 hours
 done
+
 
